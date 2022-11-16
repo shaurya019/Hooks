2aa035fbc73a34deaa95d8302379cbae7379a0e2
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2022-11-16 22:59:15 +0530

    5

--- src/component/CounterEffect.js
+++ src/component/CounterEffect.js
@@ -2,11 +2,22 @@ import React , { useState,useEffect } from 'react'
 
 function CounterEffect() {
     const [count,setcount] = useState(0);
+    const [name,setname] = useState('');
+    // useEffect(()=>{
+    //     console.log(`${count}`)
+    //     document.title = `Clicked ${count} times`
+    // })
+
+    // condition effect 
     useEffect(()=>{
+        console.log(`${count}`)
         document.title = `Clicked ${count} times`
-    })
+    },[count])
     return (
         <div>
+       <input type='text'
+       value={name}
+       onChange={e => setname(e.target.value)} />
               <button onClick={()=>setcount(count+1)}>count {count}</button>
         </div>
     )
